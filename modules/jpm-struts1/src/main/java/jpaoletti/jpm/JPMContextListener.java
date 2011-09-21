50b6d3ce410de3331b093cc1fda01420f241fef7
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-09-21 16:12:18 -0300

    jpm initialization in context

--- modules/jpm-struts1/src/main/java/jpaoletti/jpm/JPMContextListener.java
+++ modules/jpm-struts1/src/main/java/jpaoletti/jpm/JPMContextListener.java
@@ -1,9 +1,13 @@
 package jpaoletti.jpm;
 
 import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import jpaoletti.jpm.core.PresentationManager;
+import jpaoletti.jpm.util.ResourceManager;
 
 /**
  * Context listener for jPM initialization
@@ -17,11 +21,16 @@ public class JPMContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        //Load jpm-config.xml
-        //Call PresentationManager.init();
-        final InputStream is = JPMContextListener.class.getResourceAsStream("/jpm-config.xml");
-        PresentationManager.pm = new PresentationManager();
-        PresentationManager.getPm().initialize(null);
+        try {
+            final InputStream is = ResourceManager.getInputStream("jpm-config.xml");
+            final Properties properties = new Properties();
+            properties.loadFromXML(is);
+            PresentationManager.pm = new PresentationManager();
+            final PresentationManager pm = PresentationManager.getPm();
+            pm.initialize(properties);
+        } catch (Exception ex) {
+            Logger.getLogger(JPMContextListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
