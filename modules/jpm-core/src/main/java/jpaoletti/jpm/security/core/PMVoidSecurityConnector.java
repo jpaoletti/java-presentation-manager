3e2ecbcf0eb8d00533772c142265e2150f9a963d
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-09-25 23:42:40 -0300

    Void security have some sample items

--- modules/jpm-core/src/main/java/jpaoletti/jpm/security/core/PMVoidSecurityConnector.java
+++ modules/jpm-core/src/main/java/jpaoletti/jpm/security/core/PMVoidSecurityConnector.java
@@ -1,6 +1,6 @@
 package jpaoletti.jpm.security.core;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,6 +10,42 @@ import java.util.List;
  */
 public class PMVoidSecurityConnector extends PMSecurityAbstractConnector {
 
+    private static final List<PMSecurityUser> users = new ArrayList<PMSecurityUser>();
+    private static final List<PMSecurityPermission> permissions = new ArrayList<PMSecurityPermission>();
+    private static final List<PMSecurityUserGroup> groups = new ArrayList<PMSecurityUserGroup>();
+
+    static {
+
+        final PMSecurityPermission sysadmin = new PMSecurityPermission();
+        sysadmin.setName("sysadmin");
+        sysadmin.setDescription("Sysadmin");
+
+        final PMSecurityPermission useradmin = new PMSecurityPermission();
+        useradmin.setName("useradmin");
+        useradmin.setDescription("Useradmin");
+
+        permissions.add(useradmin);
+        permissions.add(sysadmin);
+
+        final PMSecurityUserGroup group = new PMSecurityUserGroup();
+        group.setName("all");
+        group.setDescription("All");
+        group.setId("all");
+        group.setActive(true);
+        group.setPermissions(permissions);
+        groups.add(group);
+
+        final PMSecurityUser user = new PMSecurityUser();
+        user.setUsername("test");
+        user.setName("test");
+        user.setActive(true);
+        user.setDeleted(false);
+        user.setChangePassword(false);
+        user.setGroups(groups);
+
+        users.add(user);
+    }
+
     @Override
     public PMSecurityUser authenticate(String username, String password) throws PMSecurityException {
         return getUser(username);
@@ -17,22 +53,17 @@ public class PMVoidSecurityConnector extends PMSecurityAbstractConnector {
 
     @Override
     public PMSecurityUser getUser(String username) throws PMSecurityException {
-        final PMSecurityUser user = new PMSecurityUser();
-        user.setUsername(username);
-        user.setName(username);
-        user.setActive(true);
-        user.setDeleted(false);
-        user.setChangePassword(false);
-        return user;
+        return users.get(0);
     }
 
     @Override
     public List<PMSecurityUser> getUsers() throws PMSecurityException {
-        return Collections.EMPTY_LIST;
+        return users;
     }
 
     @Override
     public void addUser(PMSecurityUser user) throws PMSecurityException {
+        users.add(user);
     }
 
     @Override
@@ -41,16 +72,22 @@ public class PMVoidSecurityConnector extends PMSecurityAbstractConnector {
 
     @Override
     public PMSecurityUserGroup getGroup(String id) throws PMSecurityException {
+        for (PMSecurityUserGroup group : groups) {
+            if (id.equals(group.getId())) {
+                return group;
+            }
+        }
         return null;
     }
 
     @Override
     public List<PMSecurityUserGroup> getGroups() throws PMSecurityException {
-        return Collections.EMPTY_LIST;
+        return groups;
     }
 
     @Override
     public void addGroup(PMSecurityUserGroup group) throws PMSecurityException {
+        groups.add(group);
     }
 
     @Override
@@ -59,10 +96,11 @@ public class PMVoidSecurityConnector extends PMSecurityAbstractConnector {
 
     @Override
     public void removeGroup(PMSecurityUserGroup group) throws PMSecurityException {
+        groups.remove(group);
     }
 
     @Override
     public List<PMSecurityPermission> getPermissions() throws PMSecurityException {
-        return Collections.EMPTY_LIST;
+        return permissions;
     }
 }
