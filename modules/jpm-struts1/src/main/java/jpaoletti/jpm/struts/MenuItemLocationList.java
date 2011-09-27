8e662f9c5d390788b06cb245d9bd20ae615ffa15
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-09-27 08:40:17 -0300

    New "list" location. Fix #8

---
+++ modules/jpm-struts1/src/main/java/jpaoletti/jpm/struts/MenuItemLocationList.java
@@ -0,0 +1,17 @@
+package jpaoletti.jpm.struts;
+
+import jpaoletti.jpm.menu.MenuItem;
+
+/**
+ * Location for internal links that uses loadPage javascript function. This 
+ * version take the entity id and make a link  to a list operation
+ *
+ * @author jpaoletti
+ */
+public class MenuItemLocationList extends MenuItemLocationStruts {
+
+    @Override
+    protected String buildLink(MenuItem item, Object... params) {
+        return (String) params[0] + "/list.do?pmid=" + item.getLocationValue();
+    }
+}
