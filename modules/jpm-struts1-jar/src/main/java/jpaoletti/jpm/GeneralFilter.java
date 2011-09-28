0886289e9c2a59f8555872e706b2571ce620d18a
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-09-28 20:49:43 -0300

    Modularized general filter

--- modules/jpm-struts1-jar/src/main/java/jpaoletti/jpm/GeneralFilter.java
+++ modules/jpm-struts1-jar/src/main/java/jpaoletti/jpm/GeneralFilter.java
@@ -36,41 +36,8 @@ public class GeneralFilter implements Filter, PMCoreConstants, PMStrutsConstants
         if (isIgnored(req.getRequestURI())) {
             chain.doFilter(request, response);
         } else {
-            final PresentationManager pm = PresentationManager.getPm();
-            req.setAttribute("pm", pm);
-            if (pm == null) {
-                chain.doFilter(request, response);
-                return;
-            }
-            final PMEntitySupport o = (PMEntitySupport) req.getSession().getAttribute(ENTITY_SUPPORT);
-            if (o == null) {
-                PMEntitySupport es = PMEntitySupport.getInstance();
-                es.setContext_path(req.getContextPath());
-                req.getSession().setAttribute(ENTITY_SUPPORT, es);
-            }
-            final PMSession pmsession = PMEntitySupport.getPMSession(req);
-            final String sessionId = (pmsession != null) ? pmsession.getId() : "";
-            final PMStrutsContext ctx = new PMStrutsContext(sessionId);
-            req.setAttribute("ctx", ctx);
-            ctx.setRequest(req);
-            ctx.setResponse((HttpServletResponse) response);
-            ctx.getRequest().setAttribute(PM_CONTEXT, ctx);
-            
-            for (Object object : req.getParameterMap().entrySet()) {
-                Map.Entry entry = (Map.Entry) object;
-                ctx.put("param_" + entry.getKey(), entry.getValue());
-            }
-
-            final Object pmid = ctx.getParameter("pmid");
-            ctx.put(OperationCommandSupport.PM_ID, pmid);
-            ctx.getRequest().setAttribute("pmid", pmid);
-
-            final Object item = ctx.getParameter("item");
-            ctx.put(OperationCommandSupport.PM_ITEM, item);
-            ctx.getRequest().setAttribute("item", item);
-
+            final PMStrutsContext ctx = initJPMContext(req, response);
             try {
-                ctx.getPersistenceManager(); // deprecated. Used to back compat
                 chain.doFilter(request, response);
             } catch (ServletException e) {
                 error(ctx, e);
@@ -97,7 +64,7 @@ public class GeneralFilter implements Filter, PMCoreConstants, PMStrutsConstants
     public void init(FilterConfig arg0) throws ServletException {
     }
 
-    private boolean isIgnored(String s) {
+    protected boolean isIgnored(String s) {
         if (PresentationManager.getPm() != null) {
             final String[] ignoredExtensions = PresentationManager.getPm().getCfg().getProperty("struts-ignored-extensions", "css,gif,png,jpg,js").split(",");
             for (String ext : ignoredExtensions) {
@@ -109,5 +76,41 @@ public class GeneralFilter implements Filter, PMCoreConstants, PMStrutsConstants
         } else {
             return true;
         }
+    }
+
+    protected PMStrutsContext initJPMContext(HttpServletRequest request, ServletResponse response) {
+        final PresentationManager pm = PresentationManager.getPm();
+        request.setAttribute("pm", pm);
+        if (pm == null) {
+            return null;
+        }
+        final PMEntitySupport o = (PMEntitySupport) request.getSession().getAttribute(ENTITY_SUPPORT);
+        if (o == null) {
+            PMEntitySupport es = PMEntitySupport.getInstance();
+            es.setContext_path(request.getContextPath());
+            request.getSession().setAttribute(ENTITY_SUPPORT, es);
+        }
+        final PMSession pmsession = PMEntitySupport.getPMSession(request);
+        final String sessionId = (pmsession != null) ? pmsession.getId() : "";
+        final PMStrutsContext ctx = new PMStrutsContext(sessionId);
+        request.setAttribute("ctx", ctx);
+        ctx.setRequest(request);
+        ctx.setResponse((HttpServletResponse) response);
+        ctx.getRequest().setAttribute(PM_CONTEXT, ctx);
+
+        for (Object object : request.getParameterMap().entrySet()) {
+            Map.Entry entry = (Map.Entry) object;
+            ctx.put("param_" + entry.getKey(), entry.getValue());
+        }
+
+        final Object pmid = ctx.getParameter("pmid");
+        ctx.put(OperationCommandSupport.PM_ID, pmid);
+        ctx.getRequest().setAttribute("pmid", pmid);
+
+        final Object item = ctx.getParameter("item");
+        ctx.put(OperationCommandSupport.PM_ITEM, item);
+        ctx.getRequest().setAttribute("item", item);
+        ctx.getPersistenceManager(); // deprecated. Used to back compat
+        return ctx;
     }
 }
