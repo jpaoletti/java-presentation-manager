387e4d480e93a3ab006e2826c5ba11c315c0651c
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2011-11-02 14:26:07 -0300

    Better decimal converter. Fix #16

--- modules/jpm-struts1-jar/src/main/java/jpaoletti/jpm/struts/converter/EditDecimalConverter.java
+++ modules/jpm-struts1-jar/src/main/java/jpaoletti/jpm/struts/converter/EditDecimalConverter.java
@@ -2,6 +2,7 @@ package jpaoletti.jpm.struts.converter;
 
 import java.math.BigDecimal;
 
+import java.math.RoundingMode;
 import jpaoletti.jpm.converter.ConverterException;
 import jpaoletti.jpm.core.PMContext;
 
@@ -10,20 +11,44 @@ import jpaoletti.jpm.core.PMContext;
  * <pre>
  * {@code
  * <converter class="jpaoletti.jpm.converter.EditDecimalConverter">
- *     <operationId>edit</operationId>
+ *     <properties>
+ *         <property name="separator" value="." />
+ *         <property name="decimals" value="2" />
+ *         <property name="with-null" value="false" />
+ *     </properties>
  * </converter>
  * }
  * </pre>
  * @author jpaoletti
  * */
-public class EditDecimalConverter extends EditStringConverter {
+public class EditDecimalConverter extends DefaultStrutsConverter {
 
     @Override
     public Object build(PMContext ctx) throws ConverterException {
         try {
-            return new BigDecimal((String) ctx.getFieldValue());
-        } catch (NumberFormatException e) {
+            final String separator = getConfig("separator", ".");
+            return new BigDecimal(((String) ctx.getFieldValue()).replace(separator, "."));
+        } catch (Exception e) {
             return null;
         }
+    }
+
+    @Override
+    public String visualize(PMContext ctx) throws ConverterException {
+        BigDecimal p = (BigDecimal) ctx.getFieldValue();
+        if (p == null) {
+            p = (BigDecimal) getValue(ctx.getEntityInstance(), ctx.getField());
+        }
+        final Integer decimals = Integer.parseInt(getConfig("decimals", "2"));
+        final String separator = getConfig("separator", ".");
+        final String value = (p == null) ? "" : p.setScale(
+                decimals,
+                RoundingMode.HALF_EVEN).toString().replace(".", separator);
+        ctx.setFieldValue(value);
+        return super.visualize("decimal-edit.jsp?"
+                + "&isNull=" + (p == null)
+                + "&withNull=" + getConfig("with-null", "false")
+                + "&separator=" + separator
+                + "&decimals=" + decimals);
     }
 }
---
+++ modules/jpm-struts1/src/main/webapp/converters/decimal-edit.jsp
@@ -0,0 +1,20 @@
+<%@include file="../inc/tag-libs.jsp" %>
+<c:if test="${param.withNull}">
+    <input type="checkbox" ${param.isNull ? 'checked' : ''} value="true" 
+           id="f_${param.f}_null" name="f_${param.f}_null" 
+           onclick="enable_disable('f_${param.f}', this.checked);" />
+</c:if>
+<input class="decimal-converter" alt="${param.f}mask" type="text" maxlength="${param.ml}" 
+       ${ (param.isNull and param.withNull) ? 'disabled=disabled' : ''} 
+       value="${ctx.fieldValue}" id="f_${param.f}" name="f_${param.f}" />
+<script type="text/javascript" src="${es.context_path}/js/jquery.caret.min.js"></script>
+<script type="text/javascript" src="${es.context_path}/js/jquery.decimalMask.1.1.1.min.js"></script>
+<script type="text/javascript">
+    PM_register(function(){
+        $("#f_${param.f}").decimalMask({
+            separator: "${param.separator}",
+            decSize: ${param.decimals},
+            intSize: 20
+        });
+    });
+</script>
