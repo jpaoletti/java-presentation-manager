c3ff0dbf789d4d281418909f8a8ebd5e1d9a5162
Jeronimo Paoletti <jpaoletti@jpaoletti-acer.(none)>
2012-04-10 09:36:23 -0300

    jpm database test user is created only for localhost

--- modules/jpm-hibernate/src/main/resources/jpm_test.sql
+++ modules/jpm-hibernate/src/main/resources/jpm_test.sql
@@ -1,13 +1,13 @@
 
 CREATE DATABASE `jpm_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-CREATE USER 'jpm'@'%' IDENTIFIED BY 'jpm';
+CREATE USER 'jpm'@'localhost' IDENTIFIED BY 'jpm';
 
-GRANT USAGE ON * . * TO 'jpm'@'%' IDENTIFIED BY 'jpm' WITH 
+GRANT USAGE ON *.* TO 'jpm'@'localhost' IDENTIFIED BY 'jpm' WITH 
 MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 
 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
 
-GRANT ALL PRIVILEGES ON `jpm_test` . * TO 'jpm'@'%';
+GRANT ALL PRIVILEGES ON `jpm_test`.* TO 'jpm'@'localhost';
 
 CREATE TABLE `jpm_test`.`simpleclass` (
     `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
