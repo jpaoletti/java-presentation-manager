0b076043f622513ffed8da714bbd36296ea7e312
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2012-07-15 13:45:01 -0300

    enhanced two column converter

--- modules/jpm-struts1/src/main/java/jpaoletti/jpm/struts/converter/TwoColumnConverter.java
+++ modules/jpm-struts1/src/main/java/jpaoletti/jpm/struts/converter/TwoColumnConverter.java
@@ -4,19 +4,19 @@ import jpaoletti.jpm.converter.ConverterException;
 import jpaoletti.jpm.core.PMContext;
 
 /**
- * Hack to split forms in two columns. Only for bootstrap version. 
- * 
+ * Hack to split forms in two columns. Only for bootstrap version.
+ *
  * To use this converter this two methods must be placed at represented class:
- * 
- * public String getTwocolumn() {return "ignore";}
- * public void setTwocolumn(String ignore) {}
- * 
+ *
+ * public String getTwocolumn() {return "ignore";} public void
+ * setTwocolumn(String ignore) {}
+ *
  * <converter class="jpaoletti.jpm.struts.converter.TwoColumnConverter">
  *     <properties>
  *         <property name="count" value="11" />
  *     </properties>
  * </converter>
- * 
+ *
  * @author jpaoletti
  */
 public class TwoColumnConverter extends DefaultStrutsConverter {
@@ -24,6 +24,6 @@ public class TwoColumnConverter extends DefaultStrutsConverter {
     @Override
     public Object visualize(PMContext ctx) throws ConverterException {
         final String count1 = getConfig("count", "5");
-        return super.visualize("twocolumn.jsp?count1=" + count1);
+        return super.visualize("twocolumn.jsp?count1=" + count1 + "&class1=" + getConfig("class1", "span6") + "&class2=" + getConfig("class2", "span6"));
     }
 }
--- modules/jpm-struts1/src/main/webapp/converters/twocolumn.jsp
+++ modules/jpm-struts1/src/main/webapp/converters/twocolumn.jsp
@@ -5,9 +5,9 @@
         var form = $("#control-group-${param.f}").parents("form");
         var row = jQuery('<div/>').appendTo(form).addClass("row-fluid");
         var row2 = jQuery('<div/>').appendTo(form).addClass("row-fluid");
-        var div1 = jQuery('<div/>').appendTo(row).addClass("span6");
-        var div2 = jQuery('<div/>').appendTo(row).addClass("span6");
-        var bots = jQuery('<div/>').appendTo(row2).addClass("span12 clearfix");
+        var div1 = jQuery('<div/>').appendTo(row).addClass("${param.class1}");
+        var div2 = jQuery('<div/>').appendTo(row).addClass("${param.class2}");
+        var buttons = jQuery('<div/>').appendTo(row2).addClass("span12 clearfix");
         var all = $(".control-group");
         $.each(all.slice(0,${param.count1}), function(){
             $(this).detach().appendTo(div1);
@@ -15,7 +15,7 @@
         $.each(all.slice(${param.count1}), function(){
             $(this).detach().appendTo(div2);
         });
-        form.find(".well-small").appendTo(bots);
+        form.find(".well-small").appendTo(buttons);
         $("#control-group-${param.f}").remove();
     });
 </script>
