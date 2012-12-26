7d59f1601a32e9573db90ebbdaad8a70783a2453
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2012-12-26 11:19:45 -0300

    fix in boolean converter with null

--- modules/jpm-struts1/src/main/java/jpaoletti/jpm/struts/converter/EditBooleanConverter.java
+++ modules/jpm-struts1/src/main/java/jpaoletti/jpm/struts/converter/EditBooleanConverter.java
@@ -4,9 +4,8 @@ import jpaoletti.jpm.converter.ConverterException;
 import jpaoletti.jpm.core.PMContext;
 
 /**
- * Edit a boolean. with-null property indicates a third option with "null"
- * value
- * 
+ * Edit a boolean. with-null property indicates a third option with "null" value
+ *
  * @author jpaoletti
  */
 public class EditBooleanConverter extends StrutsEditConverter {
@@ -20,17 +19,24 @@ public class EditBooleanConverter extends StrutsEditConverter {
         if (res.compareTo("false") == 0) {
             return false;
         }
-        throw new ConverterException("pm.struts.converter.invalid.boolean");
+        if (!isWithNull()) {
+            throw new ConverterException("pm.struts.converter.invalid.boolean");
+        } else {
+            return null;
+        }
     }
 
     @Override
     public String visualize(PMContext ctx) throws ConverterException {
         final Boolean p = (Boolean) getValue(ctx.getEntityInstance(), ctx.getField());
-        final boolean withnull = Boolean.parseBoolean(getConfig("with-null", "false"));
-        if (!withnull) {
+        if (!isWithNull()) {
             return super.visualize("boolean_converter.jsp?checked=" + ((p != null && p.booleanValue()) ? "checked" : ""));
         } else {
             return super.visualize("nboolean_converter.jsp?checked=" + ((p == null) ? "null" : (p.booleanValue()) ? "true" : "false"));
         }
+    }
+
+    protected boolean isWithNull() {
+        return Boolean.parseBoolean(getConfig("with-null", "false"));
     }
 }
