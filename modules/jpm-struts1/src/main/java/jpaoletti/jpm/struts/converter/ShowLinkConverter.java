8733eb4aa39f6ca3eb289cdd5f918fec6948083b
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2013-03-30 09:19:49 -0300

    link converter show nothing if other object is null

--- modules/jpm-struts1/src/main/java/jpaoletti/jpm/struts/converter/ShowLinkConverter.java
+++ modules/jpm-struts1/src/main/java/jpaoletti/jpm/struts/converter/ShowLinkConverter.java
@@ -5,8 +5,8 @@ import jpaoletti.jpm.core.PMContext;
 import jpaoletti.jpm.struts.PMEntitySupport;
 
 /**
- * Link converter shows a link with the property set in "display" as the text
- * of the link and a reference to another operation (set with "operation" poperty)
+ * Link converter shows a link with the property set in "display" as the text of
+ * the link and a reference to another operation (set with "operation" poperty)
  * with an identified referenciation to the given "entity" and the given
  * identified "property".
  *
@@ -33,8 +33,13 @@ public class ShowLinkConverter extends DefaultStrutsConverter {
         final String property = getConfig("property");
         final String display = getConfig("display");
         final Object otherObject = ctx.getPresentationManager().get(ctx.getEntityInstance(), ctx.getField().getProperty());
-        ctx.put("display", PMEntitySupport.toHtml(ctx.getPresentationManager().getAsString(otherObject, display)));
-        ctx.put("identified", property + ":" + ctx.getPresentationManager().getAsString(otherObject, property));
+        if (otherObject != null) {
+            ctx.put("display", PMEntitySupport.toHtml(ctx.getPresentationManager().getAsString(otherObject, display)));
+            ctx.put("identified", property + ":" + ctx.getPresentationManager().getAsString(otherObject, property));
+        } else {
+            ctx.put("display", "");
+            ctx.put("identified", "");
+        }
         ctx.put("other_entity", getConfig("entity"));
         ctx.put("other_operation", getConfig("operation", "show"));
         return super.visualize("link_converter.jsp?");
